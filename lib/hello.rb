0977965494ebdbc0d86dc41d795be9bee755f792
Victor Ibasco <victoribasco18@gmail.com>
2020-01-26 21:47:02 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,22 @@
-def hello_t
+# def hello_t(arr)
+#   arr.each_with_index do |ele, idx|
+#     yield arr[idx]
+#   end
+# end
 
+def hello_t(arr)
+  if block_given?
+    arr.each do |ele|
+      yield(ele)
+    end
+  else 
+    puts "Hey! No block was given!"
+  end
+  
+  arr
 end
 
 # call your method here!
+
+hello_t(['Hemmingway', 'Taurus',  'Sandy', 'Tim', 'Tammy', 'Tam', 'Yoki']);
 
